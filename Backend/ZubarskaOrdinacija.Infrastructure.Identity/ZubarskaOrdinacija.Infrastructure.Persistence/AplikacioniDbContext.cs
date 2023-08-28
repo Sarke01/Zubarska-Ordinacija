f273544194b0d2f9a11b84d1fe8d5fee9481b5d6
OgnjenT01 <ognjen.todorovic.023@gmail.com>
2023-08-28 12:24:24 +0200

    Persistance-inicijalni-commit

---
+++ Backend/ZubarskaOrdinacija.Infrastructure.Identity/ZubarskaOrdinacija.Infrastructure.Persistence/AplikacioniDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZubarskaOrdinacija.Domain.Apstrakcije.Baza;
+using ZubarskaOrdinacija.Domain.Entiteti;
+
+namespace ZubarskaOrdinacija.Infrastructure.Persistence
+{
+    public class AplikacioniDbContext : DbContext, IAplikacioniDbContext
+    {
+        public AplikacioniDbContext(DbContextOptions<AplikacioniDbContext> opcije) : base(opcije)
+        {
+
+        }
+        public DbSet<Zubar> Zubari => Set<Zubar>();
+        public DbSet<Izvestaj> Izvestaji => Set<Izvestaj>();
+        public DbSet<Termin> Termini => Set<Termin>();
+        public DbSet<Pacijent> Pacijenti => Set<Pacijent>();
+    }
+
+
+}
